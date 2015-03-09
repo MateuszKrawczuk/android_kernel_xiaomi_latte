dd8445fec9c617dd5b7d05c2ac33b96f6fe98495
Jeff McGee <jeff.mcgee@intel.com>
2015-03-09 16:06:54 -0700

    drm/i915: Export total subslice and EU counts
    Setup new I915_GETPARAM ioctl entries for subslice total and
    EU total. Userspace drivers need these values when constructing
    GPGPU commands. This kernel query method is intended to replace
    the PCI ID-based tables that userspace drivers currently maintain.
    The kernel driver can employ fuse register reads as needed to
    ensure the most accurate determination of GT config attributes.
    This first became important with Cherryview in which the config
    could differ between devices with the same PCI ID.
    
    The kernel detection of these values is device-specific and not
    included in this patch. Because zero is not a valid value for any of
    these parameters, a value of zero is interpreted as unknown for the
    device. Userspace drivers should continue to maintain ID-based tables
    for older devices not supported by the new query method.
    
    v2: Increment our I915_GETPARAM indices to fit after REVISION
        which was merged ahead of us.
    
    For: VIZ-4636
    Signed-off-by: Jeff McGee <jeff.mcgee@intel.com>
    Tested-by: Zhigang Gong <zhigang.gong@linux.intel.com>
    Acked-by: Zhigang Gong <zhigang.gong@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_dma.c
+++ drivers/gpu/drm/i915/i915_dma.c
@@ -1056,6 +1056,16 @@ static int i915_getparam(struct drm_device *dev, void *data,
 	case I915_PARAM_HAS_GET_APERTURE2:
 		value = 1;
 		break;
+	case I915_PARAM_SUBSLICE_TOTAL:
+		value = INTEL_INFO(dev)->subslice_total;
+		if (!value)
+			return -ENODEV;
+		break;
+	case I915_PARAM_EU_TOTAL:
+		value = INTEL_INFO(dev)->eu_total;
+		if (!value)
+			return -ENODEV;
+		break;
 	default:
 		DRM_DEBUG("Unknown parameter %d\n", param->param);
 		return -EINVAL;
